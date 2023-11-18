3dd31e48e5e1a36a188def64e1247e901b3b0c82
Helena Jovic <h.jovix@gmail.com>
2023-11-18 20:48:39 +0100

    Finished first task

--- hospital-equipment/src/app/app.module.ts
+++ hospital-equipment/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule, DatePipe } from '@angular/common'
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './infrastructure/router/app-routing.module';
@@ -8,6 +9,7 @@ import { AngularMaterialModule } from './angular-material/angular-material.modul
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { RegisteredUserModule } from './feature-moduls/registeredUser/registeredUser.module';
 
 @NgModule({
   declarations: [AppComponent, RegisterComponent],
@@ -19,6 +21,8 @@ import { HttpClientModule } from '@angular/common/http';
     FormsModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    CommonModule,
+    RegisteredUserModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- hospital-equipment/src/app/infrastructure/router/app-routing.module.ts
+++ hospital-equipment/src/app/infrastructure/router/app-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from '../auth/register/register.component';
+import { DisplayProfile } from 'src/app/feature-moduls/registeredUser/displayProfile/displayProfile.component';
 
-const routes: Routes = [{ path: 'register', component: RegisterComponent }];
+const routes: Routes = [{ path: 'register', component: RegisterComponent },
+{ path: 'displayProfile', component: DisplayProfile },
+        
+  ];
 
 @NgModule({
   declarations: [],
