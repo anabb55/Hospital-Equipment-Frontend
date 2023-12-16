68cb6150fdf4fa58038f4fe16a6873865affb417
anabb55 <75089113+anabb55@users.noreply.github.com>
2023-12-16 16:28:59 +0100

    Fix conflicts

--- hospital-equipment/src/app/feature-moduls/company/service/company-service.service.ts
+++ hospital-equipment/src/app/feature-moduls/company/service/company-service.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/app/env/environment.model';
@@ -11,55 +11,72 @@ import { Appointment } from 'src/app/model/appointment.model';
 import { Reservation } from 'src/app/model/reservation,model';
 
 import { EquipmentStock } from '../../model/equipmentStock.model';
-
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CompanyServiceService {
+  constructor(private http: HttpClient, private jwtHelper: JwtHelperService) {}
 
-  constructor(private http:HttpClient) { }
-
-  getAllCompanies():Observable<Company[]>{
-   return this.http.get<Company[]>(environment.apiHost+ 'companyProfile/');
+  getAllCompanies(): Observable<Company[]> {
+    return this.http.get<Company[]>(environment.apiHost + 'companyProfile/');
   }
 
-  getCompanyByAdmin(id:number):Observable<Company[]>{
-    return this.http.get<Company[]>(environment.apiHost+ 'companyProfile/byAdmin/' + id)
-      
+  getCompanyByAdmin(id: number): Observable<Company[]> {
+    return this.http.get<Company[]>(
+      environment.apiHost + 'companyProfile/byAdmin/' + id
+    );
   }
 
-  updateCompany(company:Company): Observable<Company>{
-    console.log('Poslata u servis',company)
-    return this.http.put<Company>(environment.apiHost+'companyProfile/update/'+company.id, company)
+  updateCompany(company: Company): Observable<Company> {
+    console.log('Poslata u servis', company);
+    return this.http.put<Company>(
+      environment.apiHost + 'companyProfile/update/' + company.id,
+      company
+    );
   }
 
-  getAdministratorById(id:number): Observable<CompanyAdministrator>{
-    return this.http.get<CompanyAdministrator>(environment.apiHost+'companyAdministrators/getById/'+id);
+  getAdministratorById(id: number): Observable<CompanyAdministrator> {
+    return this.http.get<CompanyAdministrator>(
+      environment.apiHost + 'companyAdministrators/getById/' + id
+    );
   }
 
-  updateCompanyAdmin(companyAdmin: CompanyAdministrator):Observable<CompanyAdministrator>{
-    return this.http.put<CompanyAdministrator>(environment.apiHost+'companyAdministrators/update/'+companyAdmin.id,companyAdmin)
+  updateCompanyAdmin(
+    companyAdmin: CompanyAdministrator
+  ): Observable<CompanyAdministrator> {
+    return this.http.put<CompanyAdministrator>(
+      environment.apiHost + 'companyAdministrators/update/' + companyAdmin.id,
+      companyAdmin
+    );
   }
 
-  getCompanyById(id:number):Observable<Company>{
-    return this.http.get<Company>(environment.apiHost+ 'companyProfile/getById/'+ id);
-   }
-
-   getEquipmentByCompanyId(companyId: number): Observable<Equipment[]> {
-    return this.http.get<Equipment[]>(`${environment.apiHost}companyProfile/company/${companyId}/equipment`);
-  }
-  
-
-   searchCompanies(name: string, city: string): Observable<Company[]> {
-    const params = new HttpParams()
-      .set('name', name)
-      .set('city', city);
-
-    return this.http.get<Company[]>(environment.apiHost + 'companyProfile/search', { params });
+  getCompanyById(id: number): Observable<Company> {
+    return this.http.get<Company>(
+      environment.apiHost + 'companyProfile/getById/' + id
+    );
   }
 
-  searchEquipmentByName(name: string, companyId: number): Observable<Equipment[]> {
+  getEquipmentByCompanyId(companyId: number): Observable<Equipment[]> {
+    return this.http.get<Equipment[]>(
+      `${environment.apiHost}companyProfile/company/${companyId}/equipment`
+    );
+  }
+
+  searchCompanies(name: string, city: string): Observable<Company[]> {
+    const params = new HttpParams().set('name', name).set('city', city);
+
+    return this.http.get<Company[]>(
+      environment.apiHost + 'companyProfile/search',
+      { params }
+    );
+  }
+
+  searchEquipmentByName(
+    name: string,
+    companyId: number
+  ): Observable<Equipment[]> {
     const url = `${environment.apiHost}companyProfile/equipment/${companyId}/search`;
     const params = { name: name };
     return this.http.get<Equipment[]>(url, { params: params });
@@ -67,55 +84,97 @@ export class CompanyServiceService {
 
   searchCompaniesByRating(grade: number): Observable<Company[]> {
     const params = new HttpParams().set('grade', grade.toString());
-    console.log("Rate je"+grade)
-  
-    return this.http.get<Company[]>(environment.apiHost + 'companyProfile/searchByRating', { params });
+    console.log('Rate je' + grade);
+
+    return this.http.get<Company[]>(
+      environment.apiHost + 'companyProfile/searchByRating',
+      { params }
+    );
   }
 
-  updateCompanyAdministrators(company:Company):Observable<Company>{
-    return this.http.put<Company>(environment.apiHost + 'companyProfile/updateAdministrators',company);
+  updateCompanyAdministrators(company: Company): Observable<Company> {
+    return this.http.put<Company>(
+      environment.apiHost + 'companyProfile/updateAdministrators',
+      company
+    );
   }
 
- generateRandomAppointments(companyId: number, date: Date): Observable<Appointment[]> {
-  const apiUrl = `${environment.apiHost}appointments/generateRandomAppointments/${companyId}?date=${date.toISOString()}`;
-  return this.http.get<Appointment[]>(apiUrl);
-}
+  generateRandomAppointments(
+    companyId: number,
+    date: Date
+  ): Observable<Appointment[]> {
+    const token = this.jwtHelper.tokenGetter();
+    console.log(token);
+    const headers = new HttpHeaders({
+      Authorization: 'Bearer ' + token,
+      'Content-Type': 'application/json',
+    });
+    const apiUrl = `${
+      environment.apiHost
+    }appointments/generateRandomAppointments/${companyId}?date=${date.toISOString()}`;
+    return this.http.get<Appointment[]>(apiUrl, { headers });
+  }
 
-saveAppointment(companyId: number, appointmentDTO: any): Observable<any> {
-  const url = `${environment.apiHost}appointments/create/${companyId}`;
-  return this.http.post(url, appointmentDTO);
-}
+  saveAppointment(companyId: number, appointmentDTO: any): Observable<any> {
+    const url = `${environment.apiHost}appointments/create/${companyId}`;
+    return this.http.post(url, appointmentDTO);
+  }
 
+  makeReservation(
+    reservationDTO: Reservation,
+    id: number
+  ): Observable<Reservation> {
+    const token = this.jwtHelper.tokenGetter();
 
-
-makeReservation(reservationDTO: Reservation, id: number): Observable<Reservation> {
-  
-  return this.http.post<Reservation>(`http://localhost:8081/api/reservation/createReservation/${id}`,reservationDTO);
-}
+    const headers = new HttpHeaders({
+      Authorization: 'Bearer ' + token,
+      'Content-Type': 'application/json',
+    });
+    return this.http.post<Reservation>(
+      `http://localhost:8081/api/reservation/createReservation/${id}`,
+      reservationDTO,
+      { headers }
+    );
+  }
   //returns all equipment that Company doesn't possess
-  getAvailableEquipmentForCompany(companyId:number):Observable<Equipment[]>{
-    return this.http.get<Equipment[]>(environment.apiHost+ 'equipments/findAvailable/' + companyId);
-   }
-
-   addEquipmentToCompany(equipmentStock:EquipmentStock):Observable<EquipmentStock[]>{
-    console.log('poslato',equipmentStock)
-    return this.http.post<EquipmentStock[]>(environment.apiHost+ 'equipmentStocks/create' ,equipmentStock);
-   }
-
-   //returns equipment that Company does possess
-   getEquipmentForCompany(companyId:number):Observable<Equipment[]>{
-    return this.http.get<Equipment[]>(environment.apiHost+ 'equipmentStocks/equipmentByCompany/' + companyId);
-   }
-
-   getEquipmentAmountForCompany(companyId:number,equipmentId:number):Observable<number>{
-  
-    return this.http.get<number>(environment.apiHost+ 'equipmentStocks/equipmentAmount/'+ companyId +'/' + equipmentId );
-   }
-
-   updateAmount(companyId:number,equipmentId:number,amount:number){
-    const url = `${environment.apiHost}equipmentStocks/update/${equipmentId}/${companyId}?amount=${amount}`;
-    return this.http.post(url, { });
+  getAvailableEquipmentForCompany(companyId: number): Observable<Equipment[]> {
+    return this.http.get<Equipment[]>(
+      environment.apiHost + 'equipments/findAvailable/' + companyId
+    );
   }
 
+  addEquipmentToCompany(
+    equipmentStock: EquipmentStock
+  ): Observable<EquipmentStock[]> {
+    console.log('poslato', equipmentStock);
+    return this.http.post<EquipmentStock[]>(
+      environment.apiHost + 'equipmentStocks/create',
+      equipmentStock
+    );
+  }
 
+  //returns equipment that Company does possess
+  getEquipmentForCompany(companyId: number): Observable<Equipment[]> {
+    return this.http.get<Equipment[]>(
+      environment.apiHost + 'equipmentStocks/equipmentByCompany/' + companyId
+    );
+  }
+
+  getEquipmentAmountForCompany(
+    companyId: number,
+    equipmentId: number
+  ): Observable<number> {
+    return this.http.get<number>(
+      environment.apiHost +
+        'equipmentStocks/equipmentAmount/' +
+        companyId +
+        '/' +
+        equipmentId
+    );
+  }
+
+  updateAmount(companyId: number, equipmentId: number, amount: number) {
+    const url = `${environment.apiHost}equipmentStocks/update/${equipmentId}/${companyId}?amount=${amount}`;
+    return this.http.post(url, {});
+  }
 }
