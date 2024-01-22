280d3cc808a5cbc455a9ccbc9aed60bc785cb385
Dajana00 <dajanaskocajic18@gmail.com>
2024-01-22 16:18:56 +0100

    Staro

---
+++ hospital-equipment/src/app/feature-moduls/model/kalendar.model.ts
@@ -0,0 +1,5 @@
+export interface Kalendar {
+    title:string,
+    startDate : Date,
+    endDate:Date
+  }
--- hospital-equipment/src/app/feature-moduls/work-calendar/work-calendar.component.ts
+++ hospital-equipment/src/app/feature-moduls/work-calendar/work-calendar.component.ts
@@ -1,6 +1,14 @@
-import { Component, OnInit } from '@angular/core';
 import { CompanyServiceService } from '../company/service/company-service.service';
 import { Company } from 'src/app/model/company.model';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Calendar, CalendarOptions, EventInput } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import { CompanyAdministrator } from 'src/app/model/companyAdministrator.model';
+import { RegisterCompanyService } from '../register-company-admin-service.service';
+import { Appointment } from 'src/app/model/appointment.model';
+import { Kalendar } from '../model/kalendar.model';
+import { dA } from '@fullcalendar/core/internal-common';
+
 
 @Component({
   selector: 'app-work-calendar',
@@ -8,17 +16,73 @@ import { Company } from 'src/app/model/company.model';
   styleUrls: ['./work-calendar.component.css']
 })
 export class WorkCalendarComponent implements OnInit{
+  @ViewChild('calendar')
 
-  loggedInUser:number =5
+  calendarEl : ElementRef;
 
-  company:Company | undefined;
+  loggedInUser:number =1
+
+  company:Company={
+    id: 0,
+    name: '',
+    address: {
+      id: 0,
+      city: '',
+      country: '',
+      street: '',
+      number: ''
+    },
+    description: '',
+    grade: 0,
+    workStartTime: {
+      hours: 0,
+      minutes: 0
+    },
+    workEndTime: {
+      hours: 0,
+      minutes: 0
+    }
+  };
   
-  constructor(private companyService:CompanyServiceService){}
+  companyAdmins:CompanyAdministrator[]=[]
+  companyAdminstrators:CompanyAdministrator[]=[]
+  appointments: Appointment[]=[]
+
+  constructor(private companyService:CompanyServiceService, private elementRef: ElementRef,private adminService: RegisterCompanyService){
+    this.calendarEl = elementRef;
+  }
   ngOnInit(): void {
+    this.initCalendar();
     this.companyService.getCompanyByAdmin(this.loggedInUser).subscribe({
       next:(result:Company[])=>{
         this.company = result[0];
-        console.log(this.company.description);
+        
+      }
+    }),
+     (error: any) => {
+      console.error(error);
+    }
+
+    this.adminService.getAllCompanyAdmins().subscribe({
+      next:(result:CompanyAdministrator[])=>{
+        this.companyAdmins = result.filter(admin=>admin.company?.description === this.company.description);
+        this.companyAdmins.forEach(admin => {
+        });
+    }
+    }),
+     (error: any) => {
+      console.error(error);
+    }
+    
+
+    this.companyService.getAllAppointment().subscribe({
+      next:(result:Appointment[])=>{
+        this.appointments = result;
+        this.appointments.forEach(a => {
+            console.log(a.appointmentStatus.toString()), // Naziv događaja (možete koristiti polje koje odgovara nazivu termina)
+            console.log(a.date+  'T' + a.endTime), // Početno vrijeme termina (format: 'YYYY-MM-DDTHH:mm:ss')
+            console.log(a.date+  'T' + a.endTime) // Završno vrijeme termina (format: 'YYYY-MM-DDTHH:mm:ss')
+        })
       }
     }),
      (error: any) => {
@@ -27,6 +91,53 @@ export class WorkCalendarComponent implements OnInit{
   
   }
 
+  calendar!: Calendar 
+
+ 
+  initCalendar(): void {
+    
+    const calendarEl: HTMLElement | null = document.getElementById('calendar') ;
+
+    const calendarOptions: CalendarOptions = {
+      plugins: [ dayGridPlugin ], // Dodajte objekt plugina, a ne samo ime
+      initialView: 'dayGridMonth',
+      events: this.generateEvents(),
+      eventTimeFormat: { // Postavite format vremena
+        hour: '2-digit',
+        minute: '2-digit',
+        meridiem: false // Opcionalno: isključuje prikaz AM/PM ako koristite 24-satni format
+      },
+      
+      // ...
+    };
+if(calendarEl !== null){
+  this.calendar = new Calendar(calendarEl, calendarOptions);
+  this.calendar.render();
+}
+
+  }
+
+rez:String[] = []
+generateEvents() {
+  console.log('USAOOOO');
+  
+  this.companyService.getAllAppointment().subscribe({
+    next:(result:Appointment[])=>{
+      this.appointments = result;
+    }
+  }),
+   (error: any) => {
+    console.error(error);
+  }
+  console.log(this.appointments.length);
+  return this.appointments.map(appointment => ({
+    title: 'Sasatnak',
+    start:appointment.date+'T'+appointment.startTime,
+    end: appointment.date+'T'+appointment.startTime    
+}));
+}
+  
+  
   
 
 }
